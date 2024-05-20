cc2acb59f99a6ecdbcba2a99a098b6f34a56d9bc
Nigel Horne <njh@bandsman.co.uk>
2024-05-20 08:43:53 -0400

    Release 0.08

--- lib/Database/Abstraction.pm
+++ lib/Database/Abstraction.pm
@@ -44,11 +44,11 @@ use constant	DEFAULT_MAX_SLURP_SIZE => 16 * 1024;	# CSV files <= than this size 
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 SYNOPSIS
 
