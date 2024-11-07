a757860fef0a990d800fb55020e66bea7354bf80
Nigel Horne <njh@bandsman.co.uk>
2024-11-07 10:48:53 -0500

    Added note about execute() on slurped data

--- lib/Database/Abstraction.pm
+++ lib/Database/Abstraction.pm
@@ -705,8 +705,8 @@ On CSV tables without no_entry, it may help to add
 "WHERE entry IS NOT NULL AND entry NOT LIKE '#%'"
 to the query.
 
-Note, this won't work on databases slurped in to memory.
-To stop that from happening, reduce max_slurp_size
+If the data have been slurped,
+this will still work by accessing that actual database.
 
 =cut
 
@@ -718,11 +718,6 @@ sub execute
 	# Ensure the 'query' parameter is provided
 	Carp::croak(__PACKAGE__, ': Usage: execute(query => $query)') 
 		unless defined $args->{'query'};
-
-	# Check if data was slurped
-	if($self->{'data'}) {
-		Carp::croak(__PACKAGE__, "Can't run exeucute on slurped data - reduce max_slurp_size");
-	}
 
 	# Get table name (remove package name prefix if present)
 	my $table = $self->{table} || ref($self);
