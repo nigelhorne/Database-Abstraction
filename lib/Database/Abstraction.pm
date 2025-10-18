51a32fefe8e25db0c94412f21d3a8dd7300907d8
Nigel Horne <njh@bandsman.co.uk>
2025-10-18 18:37:16 -0400

    Release 0.33

--- lib/Database/Abstraction.pm
+++ lib/Database/Abstraction.pm
@@ -54,11 +54,11 @@ Database::Abstraction - Read-only Database Abstraction Layer (ORM)
 
 =head1 VERSION
 
-Version 0.32
+Version 0.33
 
 =cut
 
-our $VERSION = '0.32';
+our $VERSION = '0.33';
 
 =head1 DESCRIPTION
 
