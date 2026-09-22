30041b76db3ddb5c20298ec25c8cdc0b57c079d1
Nigel Horne <njh@bandsman.co.uk>
2026-09-22 16:58:10 -0400

    Release 0.45

--- lib/Database/Abstraction.pm
+++ lib/Database/Abstraction.pm
@@ -19,7 +19,7 @@ package Database::Abstraction;
 # TODO:	Other databases e.g., Redis, noSQL, remote databases such as MySQL, PostgreSQL
 # TODO: The no_entry/entry terminology is confusing.  Replace with no_id/id_column
 # TODO: Log queries and the time that they took to execute per database
-# TODO: Add DBD::JSON if one is ever written
+# TODO: Use DBD::JSON if one is ever written
 
 use warnings;
 use strict;
